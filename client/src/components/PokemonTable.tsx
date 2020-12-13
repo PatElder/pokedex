55a9459f78e711069fbbc7c59c2eb37cb60c7ff4
Patrick <patelder95@gmail.com>
2020-12-12 22:04:20 -0500

    full base table, missing sprites

--- client/src/components/PokemonTable.tsx
+++ client/src/components/PokemonTable.tsx
@@ -1,53 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import { http } from '../util/util'
-import { PokeObj } from '../util/pokeInterface'
+import React, { useEffect, useState } from "react";
+import { http } from "../util/util";
+import { PokeObj } from "../util/pokeInterface";
 
 export function PokemonTable() {
-  const [pokemon, setPokemon] = useState([])
-  const [headers, setHeaders]: any = useState([])
+	const [pokemon, setPokemon] = useState([]);
+	const [headers, setHeaders]: any = useState([]);
 
-  useEffect(() => {
-    const tempHeaders: string[] = []
-    http('/pokemon')
-      .then((response: any) => {
-        // avoidable any with an interface
-        setPokemon(response)
+	useEffect(() => {
+		const tempHeaders: string[] = [];
+		http("/pokemon")
+			.then((response: any) => {
+				// avoidable any with an interface
+				setPokemon(response);
 
-        Object.keys(response[0]).map((key) =>
-          key !== 'base'
-            ? tempHeaders.push(key)
-            : Object.keys(response[0][key]).map((subKey) => {
-                console.log(key, subKey)
-                tempHeaders.push(subKey)
-                return tempHeaders
-              }),
-        )
-        if (headers.length === 0) {
-          setHeaders(tempHeaders)
-        }
-        return response
-      })
-      .catch((error) => error)
-  }, [headers])
-  console.log(pokemon)
-  return (
-    <div>
-      <React.Suspense fallback={<h1>Loading Pokemon...</h1>}>
-        <table>
-          <tbody>
-            <tr>
-              {headers.map((el: string, i: number) => (
-                <th key={i}>{el}</th>
-              ))}
-            </tr>
-            <tr>
-              {Object.keys(pokemon).map((el: any, i: number) => (
-                <td key={i}>{pokemon[el]}</td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
-      </React.Suspense>
-    </div>
-  )
+				Object.keys(response[0]).map((key) =>
+					key !== "base"
+						? tempHeaders.push(key)
+						: Object.keys(response[0][key]).map((subKey) => {
+								console.log(key, subKey);
+								tempHeaders.push(subKey);
+								return tempHeaders;
+						  })
+				);
+				if (headers.length === 0) {
+					setHeaders(tempHeaders);
+				}
+				return response;
+			})
+			.catch((error) => error);
+	}, [headers]);
+	return (
+		<div>
+			<React.Suspense fallback={<h1>Loading Pokemon...</h1>}>
+				<table>
+					<tbody>
+						<tr>
+							{headers.map((el: string, i: number) => (
+								<th key={i}>{el}</th>
+							))}
+						</tr>
+						{pokemon.map((el: PokeObj, i: number) => (
+							<tr>
+								<td key={i + "_" + el.id}>{el.id}</td>
+								<td key={i + "_" + el.name.english}>{el.name.english}</td>
+								<td>{el.type.map((type) => type + " ")}</td>
+								{Object.entries(el.base).map((base, entry) => (
+									<td key={base[1] + entry + i}>{base[1]}</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Suspense>
+		</div>
+	);
 }
